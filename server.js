8abc93eef0b5936f05291bc123b9e795df7dea3d
Armando <cuelarmando@yahoo.com>
2022-02-18 17:42:10 -0500

    added initial files

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const { notes } = require('./db/db.json');
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.listen(PORT, () => {
+    console.log(`API Server now on port ${PORT}!`)
+});
